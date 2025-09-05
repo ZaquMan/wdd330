4287c2547a02ad01643785c5ff74cb04a7ac4903
Zach Barnett <zacharybarnett96@gmail.com>
2025-09-04 18:38:34 -0700

    addProductToCart now uses an array

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const currentCart = getLocalStorage("so-cart") || [];
+  //Pulled the condensed version of "if currentCart is null, make it an array" from the example solution
+  currentCart.push(product);
+  setLocalStorage("so-cart", currentCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {

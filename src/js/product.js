f5fba3363ff211f0e840f1219f88c4f2a46fe038
Vikitohyah <victoriaedorh@gmail.com>
2025-09-08 14:25:57 +0100

    team.txt was created in the project root

--- src/js/product.js
+++ src/js/product.js
@@ -1,13 +1,13 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const currentCart = getLocalStorage("so-cart") || [];
+  const currentCart = getLocalStorage('so-cart') || [];
   //Pulled the condensed version of "if currentCart is null, make it an array" from the example solution
   currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
+  setLocalStorage('so-cart', currentCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -17,5 +17,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);

3faff46f02b9f34f1c1b41fcedba5466ca918633
Zach Barnett <zacharybarnett96@gmail.com>
2025-09-10 20:34:09 -0700

    Team Activty W02 Dynamic product pages

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,20 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  const currentCart = getLocalStorage("so-cart") || [];
-  //Pulled the condensed version of "if currentCart is null, make it an array" from the example solution
-  currentCart.push(product);
-  setLocalStorage("so-cart", currentCart);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
 
 // add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);

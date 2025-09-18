dc24490280ddaa3495edb6f22e7afa4340283407
Zach Barnett <zacharybarnett96@gmail.com>
2025-09-17 20:19:50 -0700

    Setup template headers and footers

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,4 +1,4 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
@@ -43,3 +43,5 @@ function renderCartTotal(cartItems) {
 }
 
 renderCartContents();
+
+loadHeaderFooter();
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 import Alert from "./Alert.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const alerts = new Alert(document.querySelector("main"));
 alerts.renderAlerts();
@@ -12,3 +13,5 @@ const element = document.querySelector(".product-list");
 const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
+
+loadHeaderFooter();

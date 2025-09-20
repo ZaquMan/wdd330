ce56f6cadeffddf01e225bdecf25ffbca8d36c3e
Matheus <silva.matheus6081@gmail.com>
2025-09-20 00:17:21 -0300

    Discount indicator-product listing

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,12 +1,27 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCartTemplate(product) {
+  const discountPercent = Math.round(
+    ((product.SuggestedRetailPrice - product.FinalPrice) /
+      product.SuggestedRetailPrice) *
+      100,
+  );
+
   return `
     <li class="product-card">
       <a href="/product_pages/index.html?product=${product.Id}">
         <img src="${product.Images.PrimaryLarge}" alt="${product.Name}">
         <h3 class="card__brand">${product.Brand.Name}</h3>
         <h2 class="card__name">${product.NameWithoutBrand}</h2>
+        ${
+          product.FinalPrice < product.SuggestedRetailPrice
+            ? `
+            <div class="price-row">
+              <span class="suggested-price">$${product.SuggestedRetailPrice}</span>
+              <span class="coupon">-${discountPercent}%</span>
+            </div>`
+            : ""
+        }
         <p class="product-card__price">$${product.FinalPrice}</p>
       </a>
     </li>

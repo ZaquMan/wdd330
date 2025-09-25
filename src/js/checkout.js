30b4db206a688e0cb27e0c6fad4dbbbb4aac4dfc
Zach Barnett <zacharybarnett96@gmail.com>
2025-09-24 20:11:36 -0700

    Team Project - Unfinished

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,3 +1,22 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, getLocalStorage } from "./utils.mjs";
 
 loadHeaderFooter();
+
+function orderSummary() {
+	const cartItems = getLocalStorage("so-cart");
+	const subtotal = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
+	const tax = subtotal * 0.06;
+	let shipping = 0;
+
+	if (cartItems.length >= 1) shipping += 10;
+	if (cartItems.length > 1) shipping += (cartItems.length - 1) * 2;
+
+	const total = subtotal + tax + shipping;
+
+	document.querySelector("#subtotal").textContent = `$${subtotal.toFixed(2)}`;
+	document.querySelector("#tax").textContent = `$${tax.toFixed(2)}`;
+	document.querySelector("#shipping").textContent = `$${shipping.toFixed(2)}`;
+	document.querySelector("#total").textContent = `$${total.toFixed(2)}`;
+}
+
+orderSummary();
